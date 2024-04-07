1380448ee844b162056ab3c560a0abb0bb3af776
DESKTOP-9MENRTJ\ymsong <ssupersam13@gmail.com>
2024-04-07 16:15:26 +0900

    fix : deleted Everything

--- Source/Capstone20_HyperReal/Capstone20_HyperReal.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Capstone20_HyperReal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Capstone20_HyperReal, "Capstone20_HyperReal" );
-
-DEFINE_LOG_CATEGORY(LogCapstone20_HyperReal)
- 
