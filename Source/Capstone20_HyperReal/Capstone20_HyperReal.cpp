eeed8b4c9bf5971aebc47537868dc6d18200014e
DESKTOP-9MENRTJ\ymsong <ssupersam13@gmail.com>
2024-04-07 16:28:45 +0900

    init : test init

---
+++ Source/Capstone20_HyperReal/Capstone20_HyperReal.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Capstone20_HyperReal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Capstone20_HyperReal, "Capstone20_HyperReal" );
+
+DEFINE_LOG_CATEGORY(LogCapstone20_HyperReal)
+ 
