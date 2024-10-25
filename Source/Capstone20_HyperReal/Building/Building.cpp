7b7abc279ee082f3679f4a82b511e79208bb0f8d
ymsong <ssupersam13@gmail.com>
2024-10-25 21:03:37 +0900

    fix : level1,2,3, 강화 메뉴 오브젝트 제거

--- Source/Capstone20_HyperReal/Building/Building.cpp
+++ Source/Capstone20_HyperReal/Building/Building.cpp
@@ -22,6 +22,7 @@ ABuilding::ABuilding() :
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SetActorTickInterval(0.5f);
 	
 	mMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
 	mHitParticle = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("HitParticle"));
